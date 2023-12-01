3fa774a76b11c46c6d908c42325ededb2b75fff9
GuiCamposxD <gui.xglpx@gmail.com>
2023-12-01 16:51:07 -0300

    melhorias no código

--- server.js
+++ server.js
@@ -1,98 +1,84 @@
 const { createServer } = require('node:http')
 const { readFile } = require('node:fs')
 
-let pagina, script, css, imagemCirculo, imagemTriangulo, imagemRetangulo, imagemPincel, imagemBorracha
-
-readFile('src/index.html', (err, data) => {
-    pagina = data
-})
-
-readFile('src/index.js', (err, data) => {
-    script = data
-})
-
-readFile('src/style.css', (err, data) => {
-    css = data
-})
-
-readFile('icons/circulo.svg', (err, data) => {
-    imagemCirculo = data
-})
-
-readFile('icons/triangulo.svg', (err, data) => {
-    imagemTriangulo = data
-})
-
-readFile('icons/retangulo.svg', (err, data) => {
-    imagemRetangulo = data
-})
-
-readFile('icons/pincel.svg', (err, data) => {
-    imagemPincel = data
-})
-
-readFile('icons/borracha.svg', (err, data) => {
-    imagemBorracha = data
-})
-
-function respond(res, statuCodigo, tipoConteudo, conteudo) {
-    res.writeHead(statuCodigo, {
+const responde = (res, tipoConteudo, conteudo) => {
+    res.writeHead(200, {
         'Content-Type': tipoConteudo,
         'Content-Length': conteudo.length
-    });
-    res.write(conteudo);
-    res.end();
+    })
+    res.write(conteudo)
+    res.end()
 }
 
-respondeHtml = (res, conteudo) => {
-    respond(res, 200, 'text/html', conteudo)
-}
-
-respondeScript = (res, conteudo) => {
-    respond(res, 200, 'text/html', conteudo)
-}
-
-respondeCss = (res, conteudo) => {
-    respond(res, 200, 'text/css', conteudo)
-}
-
-respondeSvg = (res, tipoConteudo, conteudo) => {
-    respond(res, 200, tipoConteudo, conteudo)
-}
-
-respondWithNotFound = (res) => {
-    res.writeHead(404);
-    res.end();
+respondeComNaoEncontrado = (res) => {
+    res.writeHead(404)
+    res.end()
 }
 
 handleRequest = (req, res) => {
     switch (req.url) {
         case '/':
-            respondeHtml(res, pagina);
-            break;
-        case '/script.js':
-            respondeScript(res, script);
-            break;
-        case '/style.css':
-            respondeCss(res, css);
-            break;
+            readFile('src/pages/index.html', 'utf-8', (err, data) => {
+                if (err) throw err
+
+                return responde(res, 'text/html', data)
+            })
+            break
+        case '/index.js':
+            readFile('src/index.js', 'utf-8', (err, data) => {
+                if (err) throw err
+
+                console.log(data)
+
+                return responde(res, 'text/html', data)
+            })
+            break
+        case '/styles/style.css':
+            readFile('src/styles/style.css', 'utf-8', (err, data) => {
+                if (err) throw err
+
+                console.log(data)
+
+                return responde(res, 'text/css', data)
+            })
+            break
         case '/icons/retangulo.svg':
-            respondeSvg(res, 'image/svg+xml', imagemRetangulo);
-            break;
+            readFile('icons/retangulo.svg', 'utf-8', (err, data) => {
+                if (err) throw err
+
+                return responde(res, 'image/svg+xml', data)
+            })
+            break
         case '/icons/circulo.svg':
-            respondeSvg(res, 'image/svg+xml', imagemCirculo);
-            break;
+            readFile('icons/circulo.svg', 'utf-8', (err, data) => {
+                if (err) throw err
+
+                return responde(res, 'image/svg+xml', data)
+            })
+            break
         case '/icons/triangulo.svg':
-            respondeSvg(res, 'image/svg+xml', imagemTriangulo);
-            break;
+            readFile('icons/triangulo.svg', 'utf-8', (err, data) => {
+                if (err) throw err
+
+                return responde(res, 'image/svg+xml', data)
+            })
+            break
         case '/icons/pincel.svg':
-            respondeSvg(res, 'image/svg+xml', imagemPincel);
-            break;
+            readFile('icons/pincel.svg', 'utf-8', (err, data) => {
+                if (err) throw err
+
+                return responde(res, 'image/svg+xml', data)
+            })
+            break
         case '/icons/borracha.svg':
-            respondeSvg(res, 'image/svg+xml', imagemBorracha);
-            break;
+            readFile('icons/borracha.svg', 'utf-8', (err, data) => {
+                if (err) throw err
+
+                return responde(res, 'image/svg+xml', data)
+            })
+            break
         default:
-            respondWithNotFound(res);
+            respondeComNaoEncontrado(res)
     }
 }
 
